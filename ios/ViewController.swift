e1ec6c9b6de9b2435cedab5518ffea83ac475846
Michael Ding <zhiyu.ding@gmail.com>
2018-09-28 20:02:25 +1000

    Initial Commit

---
+++ ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Zhiyu Ding on 28/9/18.
+//  Copyright © 2018 Zhiyu Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
